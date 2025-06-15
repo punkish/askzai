f3dfacee0e62f2cac5525eac2cd85da132e9a2d4
punkish <punk.kish@gmail.com>
2025-06-15 17:35:08 +0200

    now dynamically creating examples and species

--- js/utils.js
+++ js/utils.js
@@ -21,4 +21,36 @@ function toggleVisibility(el) {
     el.classList.toggle('is-visible');
 }
 
-export { $, $$, getInputText, moveCursorToEnd, toggleVisibility }
+async function getSpeciesList() {
+
+    // Pre-populated species list
+    let speciesList = [
+        'Panthera leo',
+        'Tyrannosaurus rex',
+        'Quercus alba',
+        'Apis mellifera',
+        'Canis lupus',
+        'Aquila chrysaetos',
+        'Rosa damascena',
+        'Balaenoptera musculus',
+        'Drosophila melanogaster',
+        'Acer saccharum'
+    ];
+
+    const url = `${Zai.uris.zenodeo}/v3/binomens?binomen=contains(ser)`;
+    const resp = await fetch(url);
+
+    if (resp.ok) {
+        const { query, response } = await resp.json();
+        const { count, records } = response;
+
+        if (records.length) {
+            speciesList = records.map(r => r.binomen);
+        }
+        
+    }
+    
+    return speciesList
+}
+
+export { $, $$, getInputText, moveCursorToEnd, toggleVisibility, getSpeciesList }
